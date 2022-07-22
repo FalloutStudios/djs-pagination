e2b492d7da67b160a872c4e17171f7a892b04057
GhexterCortes <ghextershumies@gmail.com>
2022-07-22 16:23:58 +0800

    update base

--- src/classes/Base.ts
+++ src/classes/Base.ts
@@ -1,17 +1,45 @@
-import { CommandInteraction, If, Interaction, Message, ModalSubmitInteraction } from 'discord.js';
+import { Awaitable, CollectedInteraction, CommandInteraction, If, Interaction, InteractionCollector, Message, MessageCollector, MessageComponentCollectorOptions, MessageComponentInteraction, MessageComponentType, ModalSubmitInteraction } from 'discord.js';
+import EventEmitter from 'events';
 import { Page } from '../types/pagination';
 
 export interface PaginationBaseOptions {
     pages: Page[];
 }
 
-export class PaginationBase<Paginated extends boolean = boolean> {
+export interface PaginationBaseEvents {
+    "pageChange": [page: Page, index: number];
+    "collectorCollect": [interaction: MessageComponentInteraction];
+    "collectorEnd": [reason: string];
+}
+
+export interface PaginationBase<Paginated extends boolean = boolean, ComponentType extends CollectedInteraction = CollectedInteraction> extends EventEmitter {
+    on<E extends keyof PaginationBaseEvents>(event: E, listener: (...args: PaginationBaseEvents[E]) => Awaitable<void>): this;
+    on<E extends string|symbol>(event: Exclude<E, keyof PaginationBaseEvents>, listener: (...args: any) => Awaitable<void>): this;
+
+    once<E extends keyof PaginationBaseEvents>(event: E, listener: (...args: PaginationBaseEvents[E]) => Awaitable<void>): this;
+    once<E extends string|symbol>(event: Exclude<E, keyof PaginationBaseEvents>, listener: (...args: any) => Awaitable<void>): this;
+
+
+    emit<E extends keyof PaginationBaseEvents>(event: E, ...args: PaginationBaseEvents[E]): boolean;
+    emit<E extends string|symbol>(event: Exclude<E, keyof PaginationBaseEvents>, ...args: any): boolean;
+
+    off<E extends keyof PaginationBaseEvents>(event: E, listener: (...args: PaginationBaseEvents[E]) => Awaitable<void>): this;
+    off<E extends string|symbol>(event: Exclude<E, keyof PaginationBaseEvents>, listener: (...args: any) => Awaitable<void>): this;
+
+    removeAllListeners<E extends keyof PaginationBaseEvents>(event?: E): this;
+    removeAllListeners(event?: string|symbol): this;
+}
+
+export class PaginationBase<Paginated extends boolean = boolean, ComponentType extends CollectedInteraction = CollectedInteraction> extends EventEmitter {
     public pages: Page[] = [];
     public currentPage: number = 0;
     public pagination!: Paginated extends true ? Message : undefined;
     public command!: Paginated extends true ? Message|Interaction : undefined;
+    public collector?: InteractionCollector<ComponentType>;
 
     constructor(options?: PaginationBaseOptions) {
+        super();
+
         this.pages = options?.pages ?? [];
     }
 
@@ -19,7 +47,30 @@ export class PaginationBase<Paginated extends boolean = boolean> {
         return this.pages[pageIndex];
     }
 
-    public isPaginated(): this is PaginationBase<true>;
+    public addPages(...pages: (Page|string)[]) {
+        if (!pages.length) return this;
+        
+        for (const page of pages) {
+            if (typeof page === 'string') {
+                this.pages.push({ content: page });
+            } else if (typeof page === 'object' && !Array.isArray(page)) {
+                this.pages.push(page);
+            } else {
+                throw new TypeError('Invalid page given');
+            }
+        }
+
+        return this;
+    }
+
+    public setPages(...pages: Page[]) {
+        this.pages = [];
+        this.addPages(...pages);
+        
+        return this;
+    }
+
+    public isPaginated(): this is PaginationBase<true, ComponentType>;
     public isPaginated(): boolean {
         return !!this.pagination && !!this.command;
     }
