8c33290fa1b1069689ef166fd4b6eed6c71405d6
Ghex <ghextershumies@gmail.com>
2022-10-30 20:37:02 +0800

    initial commit

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './classes/base/ComponentsBuilderBase.js';
-export * from './classes/base/PaginationBase.js';
-export * from './classes/builders/ButtonPaginationComponentsBuilder.js';
-export * from './classes/ButtonPagination.js';
-export * from './classes/ReactionPagination.js';
-export * from './types/pagination.js';
